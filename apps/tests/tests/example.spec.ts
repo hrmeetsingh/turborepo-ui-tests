57b221a39b69f94757352d261c80534015faf99b
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-03-31 21:07:23 -0600

    Modifying turbo to run tests too now

--- apps/tests/tests/example.spec.ts
+++ apps/tests/tests/example.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('Web app tests', async ({ page }) => {
+  await page.goto('http://127.0.0.1:3000/');
+  await expect(page).toHaveTitle('Create Turborepo');
+  await expect(page.getByRole('link', { name: 'Docs -> Find in-depth' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Learn -> Learn more' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Templates -> Choose from over 15 examples' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Deploy -> Instantly deploy' })).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Docs app tests', async ({ page }) => {
+  await page.goto('http://127.0.0.1:3001/');
+  await expect(page).toHaveTitle('Create Turborepo');
+  await expect(page.getByRole('link', { name: 'Docs -> Find in-depth' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Learn -> Learn more' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Templates -> Choose from over 15 examples' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Deploy -> Instantly deploy' })).toBeVisible();
 });
